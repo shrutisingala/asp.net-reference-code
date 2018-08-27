edd8b7230cfc800b7b1e939c2a7971e5af4a11ab
rajasri <purama.rajasri@gmail.com>
2018-08-27 18:14:38 +0530

    added repo

---
+++ WebApplication7/WebApplication7/Index.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="WebApplication7.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="Content/bootstrap.css" rel="stylesheet" />
+
+    <title></title>
+</head>
+<body>
+
+
+
+    <form id="form1" runat="server">
+
+
+        
+    <div>
+
+       
+  <div class="input-group">
+    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+    
+       <asp:TextBox ID="TextBox1" class="form-control" runat="server"></asp:TextBox><br />
+  </div>
+  <div class="input-group">
+    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+       <asp:TextBox ID="TextBox2" class="form-control"  runat="server"></asp:TextBox><br />
+  </div>
+  <div class="input-group">
+    <span class="input-group-addon">Text</span>
+     <asp:TextBox ID="TextBox3" class="form-control" runat="server"></asp:TextBox><br />
+  </div>
+              <div class="input-group">
+    <span class="input-group-addon">Text</span>
+     <asp:TextBox ID="TextBox4" class="form-control" runat="server"></asp:TextBox><br />
+  </div>
+              <div class="input-group">
+    <span class="input-group-addon">Text</span>
+     <asp:TextBox ID="TextBox5" class="form-control" runat="server"></asp:TextBox><br />
+  </div>
+       
+        <asp:Button ID="Button3" cssClass="btn btn-default" runat="server" Text="Button" OnClick="Button3_Click" /><br />
+
+    </div>
+
+        
+       <asp:Button ID="Button2" cssClass="btn btn-default" runat="server" Text="Button" OnClick="Button2_Click" />
+        <div  class="table-responsive">
+        <asp:GridView ID="GridView1" runat="server" cssClass="table table-striped">
+        </asp:GridView>
+       
+            </div>
+        <asp:TextBox ID="TextBox6" runat="server" OnTextChanged="TextBox6_TextChanged"></asp:TextBox>
+    </form>
+</body>
+</html>
---
+++ WebApplication7/WebApplication7/Index.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+namespace WebApplication7
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        SqlConnection conn;
+        String st;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            st = @"Data Source=GRAD24\MSSQLSERVER1;Initial Catalog=Sample;Integrated Security=True";
+            conn= new SqlConnection(st);
+        }
+
+    
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            
+          
+         
+            using (conn)
+            {
+                String query = "INSERT INTO Emp (FirstName, LastName, Address1, Address2, EmailID) VALUES (@FN,@LN,@A1,@A2, @email)";
+
+                using (SqlCommand command = new SqlCommand(query, conn))
+                {
+                    Response.Write("Connection MAde");
+                    command.Parameters.AddWithValue("@FN", TextBox1.Text);
+                    command.Parameters.AddWithValue("@LN", TextBox2.Text);
+                    command.Parameters.AddWithValue("@A1", TextBox3.Text);
+                    command.Parameters.AddWithValue("@A2", TextBox4.Text);
+                    command.Parameters.AddWithValue("@email", TextBox5.Text);
+
+                    conn.Open();
+                    int result = command.ExecuteNonQuery();
+
+                    // Check Error
+                    if (result < 0)
+                    {
+                        Response.Write("Connection MAde");
+                        Console.WriteLine("Error inserting data into Database!");
+                    }
+                    else
+                    {
+                        TextBox1.Text = null;
+                        TextBox2.Text = null;
+                        TextBox3.Text = null;
+                        TextBox4.Text = null;
+                        TextBox5.Text = null;
+
+                    }
+                }
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+
+            SqlCommand cmd = new SqlCommand("select * from Emp", conn);
+            conn.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            Response.Write("Connection MAde");
+            GridView1.DataSource = dr;
+            Response.Write("Connection MAde");
+            GridView1.DataBind();
+            Response.Write("Connection MAde");
+
+
+        }
+
+        protected void TextBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
